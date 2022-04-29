1bb72af8abd0316a98f09af2a8254bb0dac9510a
Vincorp21 <vibarcar@gmail.com>
2022-04-29 14:11:20 +0200

    all done at 95%

--- main.js
+++ main.js
@@ -4,11 +4,16 @@ let pass1 = document.getElementById('pwd1');
 let pass2 = document.getElementById('pwd2');
 let submit = document.getElementById('submit');
 let msg = document.getElementById('message');
+let showUser = document.getElementById('show-users');
+let resetusers = document.getElementById('reset-users');
 //let myform = document.getElementById('my-form');
 // let valpass = ^(?=.[A-Za-z])(?=.\d)[A-Za-z\d]{8,}$.test(pass1.value);
 
-console.log(submit);
-console.log(msg);
+resetusers.addEventListener('click', fdelete);
+
+// console.log(submit);
+// console.log(msg);
+// console.log(showUser);
 
 // submit.addEventListener('click', fsubmit);
 
@@ -16,8 +21,7 @@ console.log(msg);
 
 submit.addEventListener('click', fvalidate);
 
-let users = [];
-console.log(users);
+//console.log(users);
 
 /***************FUNCION VALIDACION**********/
 
@@ -29,27 +33,47 @@ function fvalidate(e) {
     pass1.value === '' ||
     pass2.value === ''
   ) {
-    msg.innerHTML = 'Please enter all fields';
+    msg.innerHTML = `<div class="alert alert-warning" role="alert">
+    Please fill all fields!
+  </div>`;
   } else if (/(\w+?@\w+?\x2E.+)/.test(mail.value) !== true) {
-    msg.innerHTML = 'Please enter a correct email';
+    msg.innerHTML = `<div class="alert alert-warning" role="alert">
+    Please enter a correct email!
+  </div>`;
+    //Please enter a correct email'
   } else if (pass1.value !== pass2.value) {
-    msg.innerHTML = 'Las dos contraseñas no son iguales';
-    // } else if ( ^(?=.[A-Za-z])(?=.\d)[A-Za-z\d]{8,}$.text(pass1.value) !== true){
-    //   msg.innerHTML = 'Mínimo 8 caracteres, al menos una letra y un número'
-    // }
+    //msg.innerHTML = 'Las dos contraseñas no son iguales';
+    //`Las dos contraseñas no son iguales`
+    msg.innerHTML = `<div class="alert alert-warning" role="alert">
+    Please, put the same passwords!
+  </div>`;
+  } else if (
+    /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/.test(pass1.value) !== true
+  ) {
+    //msg.innerHTML = 'Mínimo 8 caracteres, al menos una letra y un número';
+    msg.innerHTML = `<div class="alert alert-warning" role="alert">
+    Please, put min 8 characteres, one letter and one number!
+  </div>`;
   } else {
-    msg.innerHTML = "Yeah, it's perfect!";
+    //msg.innerHTML = "Yeah, it's perfect Mate!";
+    msg.innerHTML = `<div class="alert alert-warning" role="alert">
+    Yeah, it's perfect Mate!!
+  </div>`;
   }
   setTimeout(() => {
     msg.innerHTML = '';
   }, 3000);
 
-  fsubmit();
+  fsubmit(e);
 }
 
+let users = [];
+
 /*************FUNCIÓN AÑADIR USUARIOS*************/
-function fsubmit(j) {
-  j.preventDefault();
+function fsubmit(r) {
+  r.preventDefault();
+  let users = JSON.parse(localStorage.getItem('users')) || [];
+
   let users2 = {
     name: name.value,
     email: mail.value,
@@ -58,6 +82,33 @@ function fsubmit(j) {
   };
   users.push(users2);
   localStorage.setItem('users', JSON.stringify(users));
+  showUser.innerHTML = '';
+  showScreen();
 }
 
 /************FUNCION MOSTRAR USUARIOS*********************/
+function showScreen() {
+  let data = JSON.parse(localStorage.getItem('users'));
+  for (let i = 0; i < data.length; i++) {
+    //showUser.innerHTML += `<p>${data[i].name}</p><p>${data[i].email}</p>`;
+    showUser.innerHTML += `<div class="card" style="width:400px">
+    <img class="card-img-top" src="imgs/Jurassic_World_dom_insta.jpeg" alt="Card image">
+    <div class="card-body">
+      <h4 class="card-title">User: ${data[i].name}</h4>
+      <p class="card-text">Email: ${data[i].email} </p>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div>`;
+  }
+}
+
+showScreen();
+
+/**********FUNCTION DELETE*************/
+function fdelete() {
+  localStorage.clear();
+  //Te borras los datos que te muestra en el div
+  showUser.remove();
+  //Te borra los datos de la consola
+  console.clear();
+}
